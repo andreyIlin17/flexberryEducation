10dcdfa6bb05113ca159f4701541496f9fb093ab
andreyIlin17 <ilin.andrey171@gmail.com>
2021-07-27 03:42:04 +0500

    реализована 1 часть 1 задания

---
+++ ConsoleApp/ClassLibrary/IdGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class IdGenerator
+    {
+        private string departId;
+        public string DepartId
+        {
+            get { return departId; }
+            set
+            {
+                if (value.Length > 11 || value.Length < 11)
+                {
+                    throw new Exception("Идентификатор ведомства должен содержать 11 символов");
+                }
+                else
+                {
+                    departId = value;
+                }
+            }
+        }
+
+        public int ComplaintId { get; set; }
+
+        public IdGenerator(string departId, int complaintId)
+        {
+            DepartId = departId;
+            ComplaintId = complaintId;
+        }
+
+        public string CreateId(string departId, string complaintDate, int complaintId)
+        {
+            string id = String.Concat(departId, "_", complaintDate.ToString(), "_", complaintId.ToString());
+            return id;
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите идентификатор ведомства");
+            string departId = Console.ReadLine();
+
+            string complaintDate = DateTime.Now.ToString("d");
+
+            int complaintId = 1;
+
+            var cs = new IdGenerator(departId, complaintId);
+
+            string result = cs.CreateId(cs.DepartId, complaintDate, cs.ComplaintId);
+
+            Console.WriteLine($"Уникальный идентификатор жалобы: {result}");
+
+        }
+    }
+}
